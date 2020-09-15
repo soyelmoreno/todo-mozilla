696d03f8f3e640038847138d9ed5e55e65a16f12
Carlos Moreno <soyelmoreno@yahoo.com>
2020-09-15 12:55:37 -0700

    Add event handling. Add nanoid for unique ids.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid(5)}`, name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
-      completed={task.completed}
       name={task.name}
+      completed={task.completed}
       key={task.id}
     />
   ));
@@ -17,12 +25,14 @@ function App(props) {
     <FilterButton label={btn.label} pressed={btn.pressed} />
   ));
 
+  const taskNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">{buttons}</div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       {/* eslint-disable-next-line */}
       <ul
         role="list"
