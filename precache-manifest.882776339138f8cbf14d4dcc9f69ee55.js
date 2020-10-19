76b987722d4b97be8a2ee135ebdca708cca47ad5
Carlos Moreno <soyelmoreno@yahoo.com>
2020-10-19 15:11:48 -0700

    Updates

---
+++ precache-manifest.882776339138f8cbf14d4dcc9f69ee55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc969f9b9fd69f45fe58836e1bd5377",
+    "url": "/todo-mozilla/index.html"
+  },
+  {
+    "revision": "d999d298c9a0bc26e7da",
+    "url": "/todo-mozilla/static/css/main.220fcbac.chunk.css"
+  },
+  {
+    "revision": "7451f168ec0bc89b3d93",
+    "url": "/todo-mozilla/static/js/2.1dffee7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-mozilla/static/js/2.1dffee7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d999d298c9a0bc26e7da",
+    "url": "/todo-mozilla/static/js/main.f516abfa.chunk.js"
+  },
+  {
+    "revision": "02d167ccaf5458454dbc",
+    "url": "/todo-mozilla/static/js/runtime-main.31d7625b.js"
+  }
+]);
